7391b49a865ab1decb4170b2ee3c5ad27af2e779
figueduff <figueduff@hotmail.com>
2022-12-06 23:40:00 -0300

    update

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -10,14 +10,24 @@ import { getFirestore } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apikey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  databaseURL: import.meta.env.VITE_DATABASEURL,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID,
-  measurementId: import.meta.env.VITE_MEASUREMENTID,
+  // apikey: import.meta.env.VITE_APIKEY,
+  // authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  // databaseURL: import.meta.env.VITE_DATABASEURL,
+  // projectId: import.meta.env.VITE_PROJECTID,
+  // storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  // messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  // appId: import.meta.env.VITE_APPID,
+  // measurementId: import.meta.env.VITE_MEASUREMENTID,
+
+  apiKey: "AIzaSyAVRu73AY3aAIRxW9PSjFfLOO1KL0CGfPQ",
+  authDomain: "project-4053803933251995461.firebaseapp.com",
+  databaseURL:
+    "https://project-4053803933251995461-default-rtdb.firebaseio.com",
+  projectId: "project-4053803933251995461",
+  storageBucket: "project-4053803933251995461.appspot.com",
+  messagingSenderId: "841288400820",
+  appId: "1:841288400820:web:ace0f8caf6c7c8a02e7ce1",
+  measurementId: "G-H548K2DHZ7",
 };
 
 // Initialize Firebase
